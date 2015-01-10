08375860736de54137a11839262ab6e62c42e0c4
amutake <amutake.s@gmail.com>
2015-01-10 21:23:00 +0900

    REPL

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,10 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
 module Main where
 
 import qualified Data.Text.IO as T
+import System.Environment
+import System.IO
 
 import Imp.Parser
 import Imp.Env
@@ -8,7 +12,27 @@ import Imp.Eval
 
 main :: IO ()
 main = do
-    code <- T.getContents
+    (com : _) <- getArgs
+    case com of
+        "repl" -> repl
+        filepath -> readAndRun filepath
+
+repl :: IO ()
+repl = do
+    env <- initialEnv
+    loop env
+  where
+    loop env = do
+        putStr "imp> "
+        hFlush stdout
+        code <- T.getLine
+        case parseCode code of
+            Left err -> print err >> loop env
+            Right prog -> run env prog >> loop env
+
+readAndRun :: FilePath -> IO ()
+readAndRun filepath = do
+    code <- T.readFile filepath
     putStrLn "======== code ========"
     T.putStr code
     putStrLn "======== end ========="
